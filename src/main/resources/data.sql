5d3a495d3b770b4e2c8d4638039728c2b7cd058c
rafael <rafael@localhost>
2019-04-12 10:26:04 -0300

    Fix id erro

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO USER VALUES (1, 'rafael', 'Rafael', '123123', 'admin');
 INSERT INTO USER VALUES (2, 'helen', 'Helen'  , '123123', 'admin');
-INSERT INTO USER VALUES (2, 'bruno', 'Bruno'  , '123123', 'admin');
+INSERT INTO USER VALUES (3, 'bruno', 'Bruno'  , '123123', 'admin');
