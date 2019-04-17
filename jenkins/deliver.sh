195ed72a727a0133c7008c52bb50f0b51dfc982f
rafael <rafael@localhost>
2019-04-17 09:15:51 -0300

    Remove maven deliver file

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
-set +x
-
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-set +x
-
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
-
 echo 'Execute docker build'
 set -x
 docker build -t rafael.altagnam/spring-security .
